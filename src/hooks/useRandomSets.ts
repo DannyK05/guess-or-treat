a90886add88d22c0b37ac29fa3e21062130c94f7
Kolade <olubanjok@gmail.com>
2025-11-01 01:09:25 +0100

    fix(hooks): fix issues with how the questions are stored in the randomSets hook and rename it to useRandomSets

---
+++ src/hooks/useRandomSets.ts
@@ -0,0 +1,21 @@
+import { useEffect, useState } from "react";
+import { halloweenScenes } from "../modules/game/data";
+import type { TScene } from "../modules/game/types";
+
+export default function useRandomSets() {
+  const [returnedQuestions] = useState(() => {
+    const totalScenes = halloweenScenes.length;
+    const selectedScenes: TScene[] = [];
+
+    while (selectedScenes.length < 6) {
+      const randomIndex = Math.floor(Math.random() * totalScenes);
+      const randomScene = halloweenScenes[randomIndex];
+      if (!selectedScenes.includes(randomScene))
+        selectedScenes.push(randomScene);
+    }
+
+    return selectedScenes;
+  });
+
+  return returnedQuestions;
+}
